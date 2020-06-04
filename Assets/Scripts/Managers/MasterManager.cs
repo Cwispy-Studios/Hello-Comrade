a0fbe07880092f5dcb72aedebcc1ec54b17cf8cf
Wong Zhihao <wzhao2679@hotmail.com>
2020-06-04 14:32:22 +0000

    First commit

---
+++ Assets/Scripts/Managers/MasterManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Singletons/MasterManager")]
+public class MasterManager : SingletonScriptableObject<MasterManager>
+{
+  [SerializeField]
+  private GameSettings _gameSettings;
+  public static GameSettings GameSettings { get { return Instance._gameSettings; } }
+}
---
+++ Assets/Scripts/TestConnect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+  [SerializeField] private string gameVersion = "0.0.1";
+  private void Start()
+  {
+    print("Connecting To Server");
+    PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+    PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+    PhotonNetwork.ConnectUsingSettings();
+  }
+
+  public override void OnConnectedToMaster()
+  {
+    print("Connected To Server");
+    print(PhotonNetwork.LocalPlayer.NickName);
+  }
+
+  public override void OnDisconnected( DisconnectCause cause )
+  {
+    print("Disconnected From Server for reason " + cause.ToString());
+  }
+}
