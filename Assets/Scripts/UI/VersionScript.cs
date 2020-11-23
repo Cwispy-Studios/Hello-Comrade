c79c6f2c16a648072cf2fe75bd6e91ed3977915b
Darryl Reekers <darrylreekers@live.nl>
2020-11-23 11:05:06 +0000

    Fixed bright flickering in Test Level Enabled Post Processing on Player prefab

---
+++ Assets/Scripts/UI/VersionScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class VersionScript : MonoBehaviour
+{
+    public TextMeshProUGUI versionDisplay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        versionDisplay = GameObject.Find("VersionText").GetComponent<TextMeshProUGUI>();
+        if (versionDisplay != null)
+        {
+            versionDisplay.text = "Version: " + Application.version;
+        }
+        else
+        {
+            Debug.Log("Cant find gameobject of type TextMeshPro with name VersionText");
+        }
+    }
+}
