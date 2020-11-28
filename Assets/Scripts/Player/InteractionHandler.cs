375986451c9232e669365dc80d44eeece50db0d5
Tim Yeung <timvalkskrant@gmail.com>
2020-11-28 20:39:25 +0000

    Working lightswitches, need to be tested in multiplayer

---
+++ Assets/Scripts/Player/InteractionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CwispyStudios.HelloComrade.Scene_Interactables;
+using UnityEngine;
+
+namespace CwispyStudios.HelloComrade.Player
+{
+    public class InteractionHandler : MonoBehaviour
+    {
+        [Header("Input Settings")] 
+        [SerializeField] private float maxInteractDistance; // Max distance at which the player can interact with an item
+        
+        private Camera playerCamera;
+        private Vector3 centerVector;
+        private LayerMask interactablesMask;
+
+        private void Start()
+        {
+            playerCamera = GetComponentInChildren<Camera>();
+            centerVector = new Vector3((int) (Screen.width / 2), (int) (Screen.height / 2), 0);
+            interactablesMask = LayerMask.GetMask("Scene Interactables");
+        }
+
+        private void OnInteractLMB()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(playerCamera.ScreenPointToRay(centerVector), out hit, maxInteractDistance,
+                interactablesMask))
+            {
+                hit.collider.gameObject.GetComponent<Interactable>().LeftMouseClick();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scene Interactables/Lighting/LightSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace CwispyStudios.HelloComrade.Scene_Interactables.Lighting
+{
+    public class LightSwitch : Interactable
+    {
+        private static Quaternion switchOnPosition = Quaternion.Euler(-65, 0, 0);
+        private static Quaternion switchOffPosition = Quaternion.Euler(35, 0, 0);
+        
+        private Light[] linkedLights;
+        private PhotonView photonView;
+
+        [SerializeField] private Transform switchTransform;
+
+        private void Awake()
+        {
+            GetLinkedLights();
+            photonView = GetComponent<PhotonView>();
+        }
+
+        private void GetLinkedLights()
+        {
+            linkedLights = GetComponentsInChildren<Light>();
+        }
+
+        public override void LeftMouseClick()
+        {
+            photonView.RPC("SwitchLights", RpcTarget.All);
+            SwitchLights();
+        }
+
+        [PunRPC]
+        private void SwitchLights()
+        {
+            foreach (var linkedLight in linkedLights)
+            {
+                linkedLight.enabled = !linkedLight.enabled;
+                switchTransform.localRotation = linkedLight.enabled ? switchOnPosition : switchOffPosition;
+            }
+        }
+    }
+}
