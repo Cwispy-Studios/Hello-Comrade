d904f9f1ef5e8a3ff636bdc2e34ba7c8aaf3902a
Tim Yeung <timvalkskrant@gmail.com>
2020-12-04 13:20:04 +0000

    Optimized LightMeasurements

---
+++ Assets/Scripts/Interactables/Lighting/LightMeasurePair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CwispyStudios.HelloComrade.Interactions.Lighting
+{
+
+    [Serializable]
+    public class LightMeasurePair
+    {
+        [SerializeField] public RenderTexture renderTexture = null;
+        [SerializeField] public Camera renderCamera = null;
+    }
+}
---
+++ Assets/Scripts/Interactables/Lighting/LightMeasurer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CwispyStudios.HelloComrade.Interactions.Lighting;
+using UnityEngine;
+
+namespace CwispyStudios.HelloComrade.Interactions.Lighting
+{
+
+    public class LightMeasurer : MonoBehaviour
+    {
+
+        [SerializeField] private LightMeasurePair[] lightMeasurePairs = null;
+        private Texture2D bufferTexture;
+        private float avg = 0;
+        private WaitForEndOfFrame endOfFrame;
+
+        [SerializeField] private Vector2 lightRange = new Vector2(0, 1);
+
+        private void Awake()
+        {
+            bufferTexture = Texture2D.grayTexture;
+            endOfFrame = new WaitForEndOfFrame();
+        }
+
+        private void Update()
+        {
+            StartCoroutine(CalculateTransparency());
+        }
+
+        private IEnumerator CalculateTransparency()
+        {
+            yield return endOfFrame;
+
+
+
+            avg = 0;
+            foreach (var lightMeasurer in lightMeasurePairs)
+            {
+                lightMeasurer.renderCamera.Render();
+                RenderTexture.active = lightMeasurer.renderTexture;
+                bufferTexture.ReadPixels(new Rect(0, 0, lightMeasurer.renderTexture.width, lightMeasurer.renderTexture.height), 0, 0);
+                bufferTexture.Apply();
+                avg += bufferTexture.GetPixel(0, 0).grayscale;
+            }
+
+            RenderTexture.active = null;
+
+            avg /= lightMeasurePairs.Length;
+            print(avg.ToString());
+        }
+    }
+}
