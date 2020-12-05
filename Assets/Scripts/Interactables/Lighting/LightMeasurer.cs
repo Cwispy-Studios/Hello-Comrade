204d4b219efddabc84e63840f4ca0f9e1a7bc384
Tim Yeung <timvalkskrant@gmail.com>
2020-12-05 14:20:24 +0000

    Pre test light invisibility implementation

--- Assets/Scripts/Interactables/Lighting/LightMeasurer.cs
+++ Assets/Scripts/Interactables/Lighting/LightMeasurer.cs
@@ -2,30 +2,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using CwispyStudios.HelloComrade.Interactions.Lighting;
+using Photon.Pun;
 using UnityEngine;
 
 namespace CwispyStudios.HelloComrade.Interactions.Lighting
 {
 
-    public class LightMeasurer : MonoBehaviour
+    public class LightMeasurer : MonoBehaviourPun, IPunObservable
     {
 
         [SerializeField] private LightMeasurePair[] lightMeasurePairs = null;
+        [SerializeField] private Renderer playerMaterial = null;
         private Texture2D bufferTexture;
         private float avg = 0;
         private WaitForEndOfFrame endOfFrame;
 
-        [SerializeField] private Vector2 lightRange = new Vector2(0, 1);
+        [SerializeField] private Vector2 lightRange = new Vector2(0.05f, .3f);
+        private float lightRangeRange; // Sorry bout that
 
         private void Awake()
         {
             bufferTexture = Texture2D.grayTexture;
             endOfFrame = new WaitForEndOfFrame();
+            lightRangeRange = lightRange.y - lightRange.x;
         }
 
         private void Update()
         {
-            StartCoroutine(CalculateTransparency());
+            if (photonView.IsMine)
+                StartCoroutine(CalculateTransparency());
         }
 
         private IEnumerator CalculateTransparency()
@@ -47,7 +52,42 @@ namespace CwispyStudios.HelloComrade.Interactions.Lighting
             RenderTexture.active = null;
 
             avg /= lightMeasurePairs.Length;
-            print(avg.ToString());
+
+            if (avg < lightRange.y)
+            {
+                if (avg < lightRange.x)
+                {
+                    avg = 0;
+                }
+                else
+                {
+                    avg -= lightRange.x;
+                    avg /= lightRangeRange;
+                }
+            }
+
+            SetShaderTransparency();
+        }
+        
+        private void SetShaderTransparency()
+        {
+            playerMaterial.material.SetFloat(4, avg);
+        }
+        
+        public void OnPhotonSerializeView( PhotonStream stream, PhotonMessageInfo info )
+        {
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(avg);
+            }
+
+            else
+            {
+                // Network player, receive data
+                avg = (float)stream.ReceiveNext();
+                SetShaderTransparency();
+            }
         }
     }
 }
